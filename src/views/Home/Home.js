f63c1886a740b8fd00c34384bb60b6b61444885c
longnh01 <ngohoanglongptit@gmail.com>
2020-06-16 18:09:00 +0700

    update

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import Link from "../../modules/navigation/Link";
 import styles from './Home.module.scss';
-import Link from '../../modules/navigation/Link'
 const Home = () => (
-  <div className="fixed bg-white w-full min-h-full h-64 grid grid-cols-2 md:grid-cols-4 grid-flow-row gap-1 ">
-    {[<Link to="/github" className="bg-gray-200" ></Link>,
-    <Link to="/covid"  className="bg-gray-200" ></Link>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-300 col-span-2 sm:col-span-1 order-first md:order-none" ></div>,
-    <div className="bg-gray-300 col-span-2 sm:col-span-1 order-first md:order-none" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>,
-    <div className="bg-gray-200" ></div>].map((e,i)=>React.cloneElement(e,{...e.props,children:i}))}
+  <div className={styles.Home+" fixed bg-white w-full min-h-full h-64 grid grid-cols-2 md:grid-cols-4 grid-flow-row"}>
+    {[
+      <Link to="/github" className="bg-gray-200"></Link>,
+      <Link to="/covid" className="bg-gray-200"></Link>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-300 col-span-2 sm:col-span-1 order-first md:order-none"></div>,
+      <div className="bg-gray-300 col-span-2 sm:col-span-1 order-first md:order-none"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+      <div className="bg-gray-200"></div>,
+    ].map((e, i) => React.cloneElement(e, { ...e.props,className:`${styles.child} ${e.props.className}`, children: i }))}
+    <div className={`${styles.pointer} hidden md:block`} />
   </div>
 );
 
