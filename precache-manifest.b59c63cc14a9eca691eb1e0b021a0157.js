6877d3cade1e06a3b5a6e7181d7c37d153852b35
longnh01 <ngohoanglongptit@gmail.com>
2020-07-01 17:25:23 +0700

    Updates

---
+++ precache-manifest.b59c63cc14a9eca691eb1e0b021a0157.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6d235bd7484bba96d27dc54d1b75b4",
+    "url": "/reStat/index.html"
+  },
+  {
+    "revision": "b431f40e226617eedf0f",
+    "url": "/reStat/static/css/0.d535e3cb.chunk.css"
+  },
+  {
+    "revision": "d391e667298f0c44e365",
+    "url": "/reStat/static/css/4.cd57f3a7.chunk.css"
+  },
+  {
+    "revision": "3f2b827516d20449281d",
+    "url": "/reStat/static/css/5.9122f70d.chunk.css"
+  },
+  {
+    "revision": "85ac0e0f5009ae70decb",
+    "url": "/reStat/static/css/6.9122f70d.chunk.css"
+  },
+  {
+    "revision": "8bec9c804ad70b426745",
+    "url": "/reStat/static/css/7.eff1490c.chunk.css"
+  },
+  {
+    "revision": "0e137503214af75b1917",
+    "url": "/reStat/static/css/main.09a22fed.chunk.css"
+  },
+  {
+    "revision": "b431f40e226617eedf0f",
+    "url": "/reStat/static/js/0.dfe00840.chunk.js"
+  },
+  {
+    "revision": "799fc59a22455ae9787f160cb1809238",
+    "url": "/reStat/static/js/0.dfe00840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab11c9f1d1615c6c924a",
+    "url": "/reStat/static/js/3.00b104e0.chunk.js"
+  },
+  {
+    "revision": "1815a375049222dbc562eb1e704845e2",
+    "url": "/reStat/static/js/3.00b104e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d391e667298f0c44e365",
+    "url": "/reStat/static/js/4.77b50613.chunk.js"
+  },
+  {
+    "revision": "3f2b827516d20449281d",
+    "url": "/reStat/static/js/5.cc643472.chunk.js"
+  },
+  {
+    "revision": "85ac0e0f5009ae70decb",
+    "url": "/reStat/static/js/6.30b958f7.chunk.js"
+  },
+  {
+    "revision": "8bec9c804ad70b426745",
+    "url": "/reStat/static/js/7.4c5988b2.chunk.js"
+  },
+  {
+    "revision": "60d36627d2525e15003f",
+    "url": "/reStat/static/js/8.e3f4f916.chunk.js"
+  },
+  {
+    "revision": "b6709d9f3386c6ca6f83",
+    "url": "/reStat/static/js/9.5e2b5385.chunk.js"
+  },
+  {
+    "revision": "0e137503214af75b1917",
+    "url": "/reStat/static/js/main.90a0738d.chunk.js"
+  },
+  {
+    "revision": "cbd1a79dff97f2236762",
+    "url": "/reStat/static/js/runtime-main.e6c2b866.js"
+  }
+]);
