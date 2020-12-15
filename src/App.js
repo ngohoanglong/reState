ad9e409e66663c85f8a1ad368e9d8b1aa2ae4e2e
ngo hoang long <no.noo.nooo.yes@gmail.com>
2020-12-15 14:59:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ServiceWorker from "components/ServiceWorker";
 import LocalStringsProvider from "modules/localstrings";
-import React from "react";
+import React, { useEffect } from "react";
 import { CacheProvider } from "./modules/cache/useCache";
 import useLocation from "./modules/navigation/useLocation";
 import { LocationSubscriber } from "./modules/navigation/useLocation.Subscriber";
@@ -17,21 +17,36 @@ const Switch = ({ children }) => {
     return location && location.includes(child.props.path);
   });
   if (!element) return null;
-  return element;
+  const icon = element.props.icon;
+  return (
+    <>
+      <Head icon={icon} />
+      {element}
+    </>
+  );
 };
 const routes = (
   <Switch>
-    <Covid path="/covid" />
-    <Github path="/github" />
-    <PeriodicTable path="/periodic-table" />
-    <Portfolio path="/portfolio" />
-    <Home path="/home" />
-
-    {/* default */}
-    <Home path="/" />
+    <Covid path="/covid" icon="🤩" />
+    <Github path="/github" icon="😃" />
+    <PeriodicTable path="/periodic-table" icon="😭" />
+    <Portfolio path="/portfolio" icon="😈" />
+    <Home path="/home" icon="❤️" />
+    <Home path="/" icon="💩" />
   </Switch>
 );
-
+const Head = ({ icon = "🤩" }) => {
+  useEffect(() => {
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    link.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2212 -1 100 100%22><text y=%22.9em%22 font-size=%2290%22>${icon}</text></svg>`;
+  }, [icon]);
+  return null;
+};
 export default function App({ store }) {
   return (
     <CacheProvider initialStore={store}>
